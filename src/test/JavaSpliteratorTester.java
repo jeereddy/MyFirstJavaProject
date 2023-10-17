18a06ae26d82058e521c134c7d91d8feb3683e77
srinathJeereddy <j.srinath6323@gmail.com>
2023-10-16 22:23:57 -0500

    Java 8 spliterator initial commit.

---
+++ src/test/JavaSpliteratorTester.java
@@ -0,0 +1,11 @@
+package test;
+
+public class JavaSpliteratorTester {
+    public static void main(String[] args) {
+        /*Enumeration, Iterator and ListIterator – till Java 7, these three iterators are available in Java.
+        From Java 8, Spliterator is added to this group of iterators. As the name itself suggest,
+        Spliterator performs partitioning as well as iterating. It is most suitable for parallel programming.
+        Using old Iterators, you can iterate only collection types. But, Spliterator can be used to iterate
+        collections, arrays and streams. */
+    }
+}
